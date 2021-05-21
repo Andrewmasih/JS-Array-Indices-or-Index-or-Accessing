f3eb8a5c032427ff37b580265b3bc745058e6118
Andrew Masih <armasih@outlook.com>
2021-05-21 16:34:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/* it is a position of a item in an Array, we can access from a specific position in an Array, by using the INDEX NOTATION */
+
+namesOfEmployees = ["Ann", "Barry", "Carl", "Carl", "Edward", "Frank", "Gary", "Harry"];
+
+// use index nonation: namesArray[2], to access any item you wish
+
+
+console.log (namesOfEmployees);
+console.log (namesOfEmployees[3]);
+
